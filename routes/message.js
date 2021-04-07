0caf45afd074fbc89aa42371bd3bdb33f8601c0f
Chasnoyt <chasnoyt@gmail.com>
2021-04-07 12:02:42 +0300

    init message router - message create

--- routes/message.js
+++ routes/message.js
@@ -1,11 +1,36 @@
 var express = require('express');
 var router = express.Router();
+const Message = require("../models/message.model");
 
 /* POST message. */
 router.post('/', function(req, res, next) {
   // req.user
+  // content
+  // createdAt
+
   let status = 'process'
-  res.send({status});
+
+  // Validate request
+  if (!req.body.content) {
+    res.status(400).send({
+      message: "Content can not be empty!"
+    });
+    return;
+  }
+
+  Message.create(req.body.content)
+      .then(data => {
+        status = 'ok'
+        res.send(data);
+      })
+      .catch(err => {
+        status = 'error'
+        res.status(500).send({
+          status,
+          message:
+              err.message || "Some error occurred while creating the Message."
+        });
+      });
 });
 
 module.exports = router;
